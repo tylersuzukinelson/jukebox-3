97f3b7d45963a5241df6cfcb2a63da73531747d2
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-27 14:41:11 -0800

    Added functionality to Play button.

--- jukebox.js
+++ jukebox.js
@@ -5,9 +5,25 @@ $( document ).ready(function() {
   // text field contents.
 
   $('#song-form').submit(function(e) {
-    $('#song-queue').html( $('#song-queue').html() + '<li>' + $('#song-form input[type="text"]').val() + '</li>' );
+    $('#song-queue').append( '<li>' + $('#song-form input[type="text"]').val() + '</li>' );
     $('#song-form input[type="text"]').val('');
     e.preventDefault();
+  });
+
+  // 2. When the "Play" button is clicked:
+  //   - Start playing the top song in the queue.
+  //   - Remove the top song in the queue.
+  //   - When the song is finished playing, repeat with the next top 
+  //     song until there are no songs left to play.
+
+  $('#play-button').on('click', function() {
+    var playSongQueue = function() {
+      if ($('#song-queue li:first-child').length > 0) {
+        var nextSong = $('#song-queue li:first-child').detach().text();
+        return playSong(parseSong(nextSong), 500, playSongQueue);
+      }
+    };
+    playSongQueue();
   });
 
 });
