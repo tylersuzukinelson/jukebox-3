50322163b693ce9016403e51013696efa2943e59
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-27 13:56:33 -0800

    Added functionality to Enqueue button.

--- jukebox.js
+++ jukebox.js
@@ -1,1 +1,13 @@
-// Your code goes here.
+$( document ).ready(function() {
+
+  // When the song form is submitted, create a new list item in the 
+  // "Song Queue" list, containing the song string. ALSO, clear the 
+  // text field contents.
+
+  $('#song-form').submit(function(e) {
+    $('#song-queue').html( $('#song-queue').html() + '<li>' + $('#song-form input[type="text"]').val() + '</li>' );
+    $('#song-form input[type="text"]').val('');
+    e.preventDefault();
+  });
+
+});
