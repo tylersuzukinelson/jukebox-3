3dd46d7364f25dca846617966f9e34ff4ead2048
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-01-27 14:59:44 -0800

    Hide play button while song queue is playing

--- jukebox.js
+++ jukebox.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
 
-  // When the song form is submitted, create a new list item in the 
-  // "Song Queue" list, containing the song string. ALSO, clear the 
-  // text field contents.
+  // 1. When the song form is submitted, create a new list item in 
+  // the "Song Queue" list, containing the song string. ALSO, clear 
+  // the text field contents.
 
   $('#song-form').submit(function(e) {
     $('#song-queue').append( '<li>' + $('#song-form input[type="text"]').val() + '</li>' );
@@ -15,12 +15,18 @@ $( document ).ready(function() {
   //   - Remove the top song in the queue.
   //   - When the song is finished playing, repeat with the next top 
   //     song until there are no songs left to play.
+  // 3. When the "Play" button is clicked, slide it up. When the last 
+  // song is finished playing, slide the "Play" button back down.
 
   $('#play-button').on('click', function() {
+    var flag = true;
     var playSongQueue = function() {
+      $('#play-button').slideUp();
       if ($('#song-queue li:first-child').length > 0) {
         var nextSong = $('#song-queue li:first-child').detach().text();
         return playSong(parseSong(nextSong), 500, playSongQueue);
+      } else {
+        $('#play-button').slideDown();
       }
     };
     playSongQueue();
